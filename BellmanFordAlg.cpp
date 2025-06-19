8e143064234bde2756ec1f941facb86cde00b51e
Lukerya <l.6808561@gmail.com>
2025-06-19 14:29:14 +0200

    comments

--- BellmanFordAlg.cpp
+++ BellmanFordAlg.cpp
@@ -6,23 +6,25 @@ BellmanFordAlg::BellmanFordAlg(const GraphReader &reader) {
 }
 
 bool BellmanFordAlg::compute(int src, int dist[], int parent[]) {
-    const GraphReader &G = *graph;
+    const GraphReader &G = *graph;//reference
     int n = G.numVertices();
 
+    //rout infinity at the begining
     for (int i = 0; i < n; ++i) {
         dist[i] = INT_MAX;
         parent[i] = -1;
     }
-    dist[src] = 0;
+    dist[src] = 0; // source to source route 0
 
+    //n -1 iterations
     for (int pass = 1; pass < n; ++pass) {
         for (int u = 0; u < n; ++u) {
-            if (dist[u] == INT_MAX) continue;
+            if (dist[u] == INT_MAX) continue; //skip
             int deg = G.numNeighbors(u);
             for (int k = 0; k < deg; ++k) {
                 int v, w;
                 G.getNeighbor(u, k, v, w);
-                if (dist[u] + w < dist[v]) {
+                if (dist[u] + w < dist[v]) { //if new rout < then write new
                     dist[v] = dist[u] + w;
                     parent[v] = u;
                 }
