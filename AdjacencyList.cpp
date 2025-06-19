cd6e608ce7ea1ba1a0b2ade0b64ce85d4da55341
Lukerya <l.6808561@gmail.com>
2025-06-19 13:19:45 +0200

    Add pred verteces to the list od edges

--- AdjacencyList.cpp
+++ AdjacencyList.cpp
@@ -12,41 +12,54 @@ AdjacencyList::AdjacencyList(int Vertexes, int Edges, bool isDirected) {
         size = Edges;
     else
         size = 2 * Edges;
-    
-    head = new int[nV];
+
+    headSucc = new int[nV];
+    headPred = new int[nV];
     edges = new Edge[size];
-    nextNeighbour = new int[size];
+    nextSucc = new int[size];
+    nextPred = new int[size];
     
     for (int i = 0; i < nV; i++)  //begining: list is empty
-        head[i] = -1;
+        headSucc[i] = -1;
+
+    for (int i = 0; i < nV; i++)  //begining: list is empty
+        headPred[i] = -1;
 }
 
 AdjacencyList::~AdjacencyList(){
-    delete[] head;
+    delete[] headSucc;
+    delete[] headPred;
     delete[] edges;
-    delete[] nextNeighbour;
+    delete[] nextSucc;
+    delete[] nextPred;
 }
 
 void AdjacencyList::addEdge(int u, int v, int w) {
     if (used >= size) return; // try too not overbook
     edges[used].destination = v;
     edges[used].weight = w;
-    nextNeighbour[used] = head[u]; //head = i of the nei. remember existing neighbour = -1 - no neighbour
-    head[u] = used;                //where to look, new link is the first now
+    edges[used].source      = u;
+    nextSucc[used] = headSucc[u]; //headSucc = i of the nei. remember existing neighbour = -1 - no neighbour
+    headSucc[u] = used;                //where to look, new link is the first now
+    nextPred[used] = headPred[v];
+    headPred[v] = used;
     used++;
 }
 
 void AdjacencyList::print() const{
-    for (int v = 0; v< nV; v++){
-        cout << v << ": ";
-        int i = head[v];
-        while(i != -1){
-            cout << "("
-                 << edges[i].destination << ", "
-            << edges[i].weight << ") ";
-            i = nextNeighbour[i];
+    std::cout << "succ || pred\n";
+    for (int u = 0; u < nV; ++u) {
+        std::cout << u << ":";
+        for (int i = headSucc[u]; i != -1; i = nextSucc[i]) {
+            std::cout << " (" << edges[i].destination
+                      << "," << edges[i].weight << ")";
         }
-        cout << endl;
+        std::cout << " || ";
+        for (int i = headPred[u]; i != -1; i = nextPred[i]) {
+            std::cout << " (" << edges[i].source
+                      << "," << edges[i].weight << ")";
+        }
+        std::cout << "\n";
     }
 }
 
